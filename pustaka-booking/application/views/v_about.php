abc6d86a2d83ad2ba961ece9033db2fbe1070a8b
TommyAlfianto <tommya405@gmail.com>
2021-10-29 10:49:59 +0700

    pertemuan 4

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,19 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Tommy</li>
+        <li>Nama Belakang : Nawawi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Berenang</li>
+    </ul>
+</section>

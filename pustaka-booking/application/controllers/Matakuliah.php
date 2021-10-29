e378ada538a15db66c3c51ef9a3b5c0497460b37
TommyAlfianto <tommya405@gmail.com>
2021-10-29 12:51:58 +0700

    pertemuan 5

--- pustaka-booking/application/controllers/Matakuliah.php
+++ pustaka-booking/application/controllers/Matakuliah.php
@@ -7,6 +7,29 @@ class Matakuliah extends CI_Controller
     }
     public function cetak()
     {
+        $this->form_validation->_rules('kode'), 'Kode Matakuliah', 'required|min_length[3]', [
+            'required' => 'Kode Matakuliah Harus diisi',
+            'min_lenght' => 'Kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama', 'Matakuliah', 'required|min_length[3]',[
+            'required' => 'Nama Matakuliah Harus diisi',
+            'min_lenght' => 'Nama terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
+    }
+}
         $data = [
             'kode' => $this->input->post('kode'),
             'nama' => $this->input->post('nama'),
